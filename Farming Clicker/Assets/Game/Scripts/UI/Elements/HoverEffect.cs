7c6d4a14003df703002fc2e561ed628fcdd48d9f
QooBoo <qoolbones@gmail.com>
2023-03-13 18:43:05 +0300

    UI Layout

---
+++ Farming Clicker/Assets/Game/Scripts/UI/Elements/HoverEffect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Game.Scripts.UI.Elements
+{
+    public class HoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private Outline _outline;
+
+        private void Awake() => 
+            _outline.enabled = false;
+
+        public void OnPointerEnter(PointerEventData eventData) => 
+            _outline.enabled = true;
+
+        public void OnPointerExit(PointerEventData eventData) => 
+            _outline.enabled = false;
+    }
+}
